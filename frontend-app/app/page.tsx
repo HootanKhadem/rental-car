d33f67f7e1bd21d3f3207a879ddec4636253c9a4
Yashar_Talebi <yashar.taleby@gmail.com>
2026-08-08 21:38:17 +0330

    Add footer section

--- frontend-app/app/page.tsx
+++ frontend-app/app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@/src/features/hero/Hero.server";
 import Assistant from "@/src/features/assistant/Assistant.server";
 import Catalog from "@/src/features/catalog/Catalog.server";
 import Membership from "@/src/features/membership/Membership.server";
+import Footer from "@/src/features/footer/Footer.server";
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Assistant />
       <Catalog />
       <Membership />
+      <Footer />
     </main>
   );
 };
